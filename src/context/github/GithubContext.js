5c4d16e257da081d0a561a303fe527dcfbec8424
jenny-hub90 <jumbledjenny19@gmail.com>
2023-05-11 19:57:41 +0545

    getting user repository lists and respository items

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -11,6 +11,7 @@ export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
         user: {},
+        repos:[],
         loading: false,
     }
 
@@ -61,6 +62,28 @@ export const GithubProvider = ({children}) => {
 
        
     }
+//get user repos
+    const getUserRepos = async (login) => {
+        setLoading()
+
+        const params = new URLSearchParams({
+            sort:'created',
+            per_page:10
+        })
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`,{
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+
+        const data = await response.json()
+
+        dispatch({
+            type: 'GET_REPOS',
+            payload: data,
+        })
+    }
 
    //clear users
    const clearUsers = () => dispatch({ type: 'CLEAR_USERS'})
@@ -74,9 +97,11 @@ export const GithubProvider = ({children}) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos
     }}>
 
         {children}
